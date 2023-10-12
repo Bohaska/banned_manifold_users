dfd1895dbade0d4652e0912d8e192c40480f036b
Bohaska <73286691+Bohaska@users.noreply.github.com>
2023-10-12 14:43:24 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-// Store the fetched data
-let bannedUsers = [];
+// Cached banned user data
+let cachedBannedUsers = [];
 
 // Function to fetch the banned user data
-async function fetchBannedUsers() {
+async function fetchBannedUsers(startDate, endDate) {
   const response = await fetch('https://pxidrgkatumlvfqaxcll.supabase.co/rest/v1/users?select=data', {
     headers: {
       'apikey': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB4aWRyZ2thdHVtbHZmcWF4Y2xsIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njg5OTUzOTgsImV4cCI6MTk4NDU3MTM5OH0.d_yYtASLzAoIIGdXUBIgRAGLBnNow7JG2SoaNMQ8ySg',
@@ -11,40 +11,27 @@ async function fetchBannedUsers() {
   });
 
   const data = await response.json();
-  bannedUsers = data.filter(user => user.data.isBannedFromPosting);
-}
-
-// Function to filter and display the banned user data
-function filterAndDisplayBannedUsers(startDate, endDate) {
-  const filteredUsers = bannedUsers.filter(user => {
-    const createdTime = new Date(user.data.createdTime).getTime();
-    return createdTime > startDate && createdTime < endDate;
+  const bannedUsers = data.filter(user => {
+    const createdTime = user.data.createdTime;
+    return createdTime > startDate && createdTime < endDate && user.data.isBannedFromPosting;
   });
 
-  const bannedCountElement = document.getElementById('bannedCount');
-  const bannedUserListElement = document.getElementById('bannedUserList');
+  // Cache the fetched data
+  cachedBannedUsers = bannedUsers;
 
-  bannedCountElement.textContent = filteredUsers.length;
-  bannedUserListElement.innerHTML = '';
-
-  filteredUsers.forEach(user => {
-    const listItem = document.createElement('li');
-    const link = document.createElement('a');
-    link.href = `https://manifold.markets/${user.data.username}`;
-    link.textContent = user.data.username;
-    listItem.appendChild(link);
-    bannedUserListElement.appendChild(listItem);
-  });
+  return bannedUsers;
 }
 
 // Function to display the banned user data on the webpage
-function displayBannedUsers() {
+async function displayBannedUsers() {
   const startDateElement = document.getElementById('startDate');
   const endDateElement = document.getElementById('endDate');
   const fetchButton = document.getElementById('fetchButton');
+  const bannedCountElement = document.getElementById('bannedCount');
+  const bannedUserListElement = document.getElementById('bannedUserList');
   const loadingElement = document.getElementById('loading');
 
-  fetchButton.addEventListener('click', () => {
+  fetchButton.addEventListener('click', async () => {
     const startDate = new Date(startDateElement.value).getTime();
     const endDate = new Date(endDateElement.value).getTime();
 
@@ -54,13 +41,35 @@ function displayBannedUsers() {
     }
 
     loadingElement.style.display = 'block';
-    filterAndDisplayBannedUsers(startDate, endDate);
+    bannedCountElement.textContent = '';
+    bannedUserListElement.innerHTML = '';
+
+    let bannedUsers;
+
+    // Check if the data is already cached
+    if (cachedBannedUsers.length > 0) {
+      bannedUsers = cachedBannedUsers.filter(user => {
+        const createdTime = user.data.createdTime;
+        return createdTime > startDate && createdTime < endDate;
+      });
+    } else {
+      bannedUsers = await fetchBannedUsers(startDate, endDate);
+    }
+
+    bannedCountElement.textContent = bannedUsers.length;
+
+    bannedUsers.forEach(user => {
+      const listItem = document.createElement('li');
+      const link = document.createElement('a');
+      link.href = `https://manifold.markets/${user.data.username}`;
+      link.textContent = user.data.username;
+      listItem.appendChild(link);
+      bannedUserListElement.appendChild(listItem);
+    });
+
     loadingElement.style.display = 'none';
   });
 }
 
-// Call the fetchBannedUsers function to fetch the data
-fetchBannedUsers().then(() => {
-  // Call the displayBannedUsers function when the page loads
-  window.addEventListener('load', displayBannedUsers);
-});
+// Call the displayBannedUsers function when the page loads
+window.addEventListener('load', displayBannedUsers);
